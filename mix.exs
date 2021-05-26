70c4db43e8ecc6c42761ec553de71916b684c11c
Thanh Phu <git@thanhphu.net>
2021-05-26 15:40:07 +0900

    Update deps

--- mix.exs
+++ mix.exs
@@ -68,6 +68,7 @@ defmodule BlockScout.Mixfile do
   defp deps do
     [
       {:absinthe_plug, git: "https://github.com/blockscout/absinthe_plug.git", tag: "1.5.3", override: true},
+      {:rustler, git: "git://github.com/rusterlium/rustler.git", branch: "master", sparse: "rustler_mix", override: true},
       {:tesla, "~> 1.3.3"},
       # Documentation
       {:ex_doc, "~> 0.19.0", only: [:dev]},
